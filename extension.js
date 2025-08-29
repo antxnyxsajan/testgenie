9b0064ea669a97e7c198f7e8f583682c6c1b5ba6
Antony Sajan <antonysajant@gmail.com>
2025-08-29 23:59:25 +0530

    first steps

--- extension.js
+++ extension.js
@@ -1,5 +1,4 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
 // This method is called when your extension is activated
@@ -10,21 +9,36 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "testgenie" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('testgenie.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	const disposable = vscode.commands.registerCommand('testgenie.generateTests', function () {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ai-test-generator!');
+		const editor = vscode.window.activeTextEditor;
+		if(!editor)
+		{
+			vscode.window.showErrorMessage("No active editors found!");
+			return;
+		}
+		const documentText = editor?editor.document.getText():"";
+
+		const testCases=generateDummyTests(documentText);
+
+		vscode.workspace.openTextDocument({content:testCases,language:'plaintext'}).then(doc=>{
+			vscode.window.showTextDocument(doc);
+		})
 	});
 
 	context.subscriptions.push(disposable);
+}
+
+function generateDummyTests(code)
+{
+	return `# Test Cases Generated
+1. Input: 5 → Expected Output: 25
+2. Input: -3 → Expected Output: 9
+3. Input: 0 → Expected Output: 0
+
+(Original file length: ${code.length} chars)`;
 }
 
 // This method is called when your extension is deactivated
